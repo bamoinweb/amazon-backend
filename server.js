222669a64c724770a218e0a2502e80d00458da0a
bamoinweb <baranchr87@gmail.com>
2026-07-19 19:48:11 +0330

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express=require('express');
+const cors=require('cors')
 const fs=require('fs');
 const app=express();
+app.use(cors());
 app.use(express.json());
 app.get('/products',(req,res)=>{
    const products=JSON.parse(fs.readFileSync('products.json','utf8'));
